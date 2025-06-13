6d3445298e82d4cc1ee542b19b0f3c40f5c64af9
Abbby1007 <Abigailloken25@gmail.com>
2025-06-13 21:57:34 +0900

    more update

--- 1Level.js
+++ 1Level.js
@@ -1,9 +1,25 @@
 
+// const resultsDisplay = document.getElementById('results');
+
+// function randomNum() {
+
+//     var num = Math.floor(Math.random() * 10);
+//     resultsDisplay.textContent = ` Number Generated: ${num}`;
+// }
+
+// let username;
+// const resultsDisplay = document.getElementById('results');
+// document.getElementById("mySubmit").onclick = function () {
+//     username = document.getElementById("myText").value;
+//     resultsDisplay.textContent = `${username}`;
+// };
+let username;
 const resultsDisplay = document.getElementById('results');
 
-function randomNum() {
+let goOn
+function mission1() {
 
-    var num = Math.floor(Math.random() * 10);
-    resultsDisplay.textContent = ` Number Generated: ${num}`;
+
+    username = document.getElementById("myText").value;
+    resultsDisplay.textContent = `${username}`;
 }
-
